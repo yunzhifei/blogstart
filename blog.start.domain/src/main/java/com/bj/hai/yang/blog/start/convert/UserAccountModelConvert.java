526f4097da18a57cfafe0e4cacf63dfddb9fca8c
yunzhifei <yunzhifei@maoyan.com>
2018-08-27 22:05:28 +0800

    convert

--- blog.start.domain/src/main/java/com/bj/hai/yang/blog/start/convert/UserAccountModelConvert.java
+++ blog.start.domain/src/main/java/com/bj/hai/yang/blog/start/convert/UserAccountModelConvert.java
@@ -1,4 +1,13 @@
-package com.bj.hai.yang.blog.start;
+package com.bj.hai.yang.blog.start.convert;
+
+import com.bj.hai.yang.blog.start.dto.UserAccountDO;
+import com.bj.hai.yang.blog.start.model.UserAccountModel;
 
 public class UserAccountModelConvert {
+    public static UserAccountModel convertToModel(UserAccountDO userAccountDO) {
+        UserAccountModel userAccountModel = new UserAccountModel();
+        userAccountModel.setId(userAccountDO.getId());
+        userAccountModel.setName(userAccountDO.getName());
+        return userAccountModel;
+    }
 }
--- blog.start.domain/src/main/java/com/bj/hai/yang/blog/start/model/UserAccountModel.java
+++ blog.start.domain/src/main/java/com/bj/hai/yang/blog/start/model/UserAccountModel.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class UserAccountModel {
     private int id;
+    private String name;
 }
