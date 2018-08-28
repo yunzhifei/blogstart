ab76f421645cafbaac1cbf1588392dd02cfd330a
yunzhifei <yunzhifei@maoyan.com>
2018-08-28 15:56:21 +0800

    多线程task

--- blog.start.provider/src/test/java/com/bj/hai/yang/blog/start/test/SpringBootUnitTest.java
+++ blog.start.provider/src/test/java/com/bj/hai/yang/blog/start/test/SpringBootUnitTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Test {
+public class SpringBootUnitTest {
 
     @Autowired
     private IUserAccountService userAccountService;
---
+++ blog.start.task/src/main/java/com/bj/hai/yang/start/task/config/TaskConfig.java
@@ -0,0 +1,15 @@
+package com.bj.hai.yang.start.task.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class TaskConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+    }
+}
