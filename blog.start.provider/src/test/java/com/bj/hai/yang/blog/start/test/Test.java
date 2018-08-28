198c962bc4469d0beda8cbd925270916ddfa73c1
yunzhifei <yunzhifei@maoyan.com>
2018-08-28 15:23:29 +0800

    单元测试和task模块建立

---
+++ blog.start.provider/src/test/java/com/bj/hai/yang/blog/start/test/Test.java
@@ -0,0 +1,22 @@
+package com.bj.hai.yang.blog.start.test;
+
+import com.bj.hai.yang.blog.start.model.UserAccountModel;
+import com.bj.hai.yang.blog.start.service.IUserAccountService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+
+    @Autowired
+    private IUserAccountService userAccountService;
+
+    @org.junit.Test
+    public void test() {
+        UserAccountModel userAccountModel = userAccountService.selectById(1);
+        System.out.println("userAccountModel = " + userAccountModel);
+    }
+}
