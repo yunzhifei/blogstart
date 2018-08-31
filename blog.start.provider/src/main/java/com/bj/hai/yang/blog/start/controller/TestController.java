646fcbba5d6375922a5d0465f1be4f380e961a63
yunzhifei <yunzhifei_chd@139.com>
2018-08-31 22:13:21 +0800

    thread

--- blog.start.provider/src/main/java/com/bj/hai/yang/blog/start/controller/TestController.java
+++ blog.start.provider/src/main/java/com/bj/hai/yang/blog/start/controller/TestController.java
@@ -2,9 +2,13 @@ package com.bj.hai.yang.blog.start.controller;
 
 import com.bj.hai.yang.blog.start.model.UserAccountModel;
 import com.bj.hai.yang.blog.start.service.IUserAccountService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class TestController {
@@ -12,10 +16,16 @@ public class TestController {
     @Autowired
     private IUserAccountService userAccountService;
 
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+    ;
+
     @RequestMapping("/")
     public String test() {
         UserAccountModel userAccountModel = userAccountService.selectById(1);
         System.out.println("userAccountModel = " + userAccountModel);
-        return "";
+        for (int i = 0; i < 6; i++) {
+            executorService.execute(new TestThread(userAccountService));
+        }
+        return "( " + "test" + " )";
     }
 }
