7c7ecfec0cd658a79c8f3f15710d69c07a2a1144
yunzhifei <yunzhiifei@maoyan.com>
2019-02-12 18:56:03 +0800

    打包war 配置

--- blog.start.provider/src/main/java/com/bj/hai/yang/blog/start/AppStarter.java
+++ blog.start.provider/src/main/java/com/bj/hai/yang/blog/start/AppStarter.java
@@ -3,15 +3,22 @@ package com.bj.hai.yang.blog.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ImportResource(locations = "classpath:mybatis-config.xml")
 @EnableScheduling
-public class
-AppStarter {
+public class AppStarter  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppStarter.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
